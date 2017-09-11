2463bcec5a4bb495e7beef0fd800178571c33fa4
Sokunthel <sokunthel@gmail.com>
2017-09-11 16:44:15 -0700

    Add comments in models

--- app/models/heart.rb
+++ app/models/heart.rb
@@ -1,4 +1,7 @@
 class Heart < ActiveRecord::Base
+  #This allows us to use @heart.post in hearts_controller
   belongs_to :post
+
+  #post_id is needed to create a heart record
   validates :post_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  #This allows us to use post.heart.count in _post
   has_many :heart
 end
